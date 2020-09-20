2f0fcfd2d1e2eaae3bb748a57194bd9f1732b51a
yalitzab <yalitzabueno@gmail.com>
2020-09-20 16:21:10 -0700

    add drop down options of states, update input mobile and add send button

--- assets/js/text_container.js
+++ assets/js/text_container.js
@@ -1,3 +1,47 @@
-$(document).ready(function(){
-    $('select').formSelect();
-  });
+// create array to hold mobile phone for saving to local storage
+var mobilePhone = []
+
+$(".send-btn").on("click", function(){
+ 
+  var searchStates = $(".dropdown-content").val()
+  console.log()
+  airQ(searchStates)
+})
+
+
+
+
+function airQ(searchStates) {
+  fetch("http://api.airvisual.com/v2/states?q=" + searchStates + "country=USA&key=92595417-3c50-41fb-9446-57d4c55dc678")
+  .then(function(response){
+    return response.json();
+  })
+  .then (function() {
+    console.log(searchStates)
+
+  
+
+  })
+}
+
+
+
+
+
+//   $(document).ready(function(state){
+//       $('#states').state();
+//     });
+
+// var instance = M.Dropdown.getInstance(elem);
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.dropdown-trigger');
+//   var instances = M.Dropdown.init(elems, options);
+
+//   console.log(Dropdown)
+// });
+
+
+$('select').formSelect();
+M.updateTextFields();
+    // //drop down list states jquery
+    
