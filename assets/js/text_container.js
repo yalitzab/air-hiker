1a7c17da8b7c0749715ee9b359301c669d4b2768
yalitzab <yalitzabueno@gmail.com>
2020-09-21 19:07:22 -0700

    add RingCental API

--- assets/js/text_container.js
+++ assets/js/text_container.js
@@ -24,24 +24,38 @@ function airQ(searchStates) {
   })
 }
 
-
-
-
-
-//   $(document).ready(function(state){
-//       $('#states').state();
-//     });
-
-// var instance = M.Dropdown.getInstance(elem);
-// document.addEventListener('DOMContentLoaded', function() {
-//   var elems = document.querySelectorAll('.dropdown-trigger');
-//   var instances = M.Dropdown.init(elems, options);
-
-//   console.log(Dropdown)
-// });
-
-
 $('select').formSelect();
 M.updateTextFields();
-    // //drop down list states jquery
-    
+   
+function smsText(){
+  fetch("	https://platform.devtest.ringcentral.com")
+}   
+
+// https://developers.ringcentral.com/my-account.html#/applications
+// Find your credentials at the above url, set them as environment variables, or enter them below
+
+// PATH PARAMETERS
+const accountId = '<ENTER VALUE>';
+const extensionId = '<ENTER VALUE>';
+
+// POST BODY
+const body = {
+    from: {
+        phoneNumber: '<ENTER VALUE>'
+    },
+    to: [
+        {
+            phoneNumber: '<ENTER VALUE>'
+        },
+    ],
+    text: '<ENTER VALUE>'
+};
+
+const SDK = require('ringcentral');
+const rcsdk = new SDK({server: process.env.serverURL, appKey: process.env.clientId, appSecret: process.env.clientSecret});
+const platform = rcsdk.platform();
+platform.login({ username: process.env.username, extension: process.env.extension, password: process.env.password }).then(() => {
+    platform.post(`/restapi/v1.0/account/${accountId}/extension/${extensionId}/sms`, body).then((r) => {
+        // PROCESS RESPONSE
+    });
+});
