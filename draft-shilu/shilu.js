aa51c8d0c4078bff48911d9dcd73358009c78e25
shilu <zoushilu@gmail.com>
2020-09-23 02:48:37 -0700

    added header and footer,radio button, results invisible before search

---
+++ draft-shilu/shilu.js
@@ -0,0 +1,17 @@
+// Send an APi requestwith the search city
+// see the data
+//Data Example
+var data = [[{car: "truck"}],[{year: 2008}]]
+// find the piece of the data we would want
+// set  variables to to the data we want
+//ex..
+var myCar = data[0][0].car
+var theYear = data[0][0].year
+
+//use the variable to set the html
+document.getElementById("cityName").innerHTML = myCar
+
+
+
+
+/// id= "cityname"
