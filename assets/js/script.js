09b2ca1b19ba75db1ddcca37450f90b558a837a3
Ken Marchand <marchand_kenneth@smc.edu>
2020-09-23 20:18:23 -0700

    Added trail difficulty logic and cleaned up the code some

---
+++ assets/js/script.js
@@ -0,0 +1,206 @@
+
+//var slider = document.getElementById("myRange");
+//var output = document.getElementById("demo");
+var airContainerEl = document.querySelector("#air-container");
+var locationInputEl = document.querySelector("#location-input");
+var resultsBox = document.querySelector("#results");
+var inputEl = document.querySelector("#input-value");
+var radios = document.getElementsByName("style2");
+var lat = 0;
+var lon = 0;
+var difficulty = "";
+var requestedDifficulty=""
+// hide the result
+document.querySelector("#resultRow").style.display = "none";
+
+
+var getAirQuality = function (searchLocation) {
+
+  var apiUrl =
+    "http://api.airvisual.com/v2/city?city=" +
+    searchLocation +
+    "&state=California&country=USA&key=9e6807b2-9614-47e1-b226-73369e760983";
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+  
+    // request was successful
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayAirData(data);
+        //console.log(data);
+        lon = data.data.location.coordinates[0];
+        lat = data.data.location.coordinates[1];
+        //console.log(lat);
+        //console.log(lon);
+        getHikeData();
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+};
+
+// getAirQuality();
+
+var displayAirData = function (airdata) {
+  // check if api returned any repos
+
+  if (airdata.length === 0) {
+    //repoContainerEl.textContent = "No repositories found.";
+    alert("No trails found");
+    return;
+  }
+
+  //var repoName = airdata[i].owner.login + "/" + repos[i].name;
+  //console.log(airdata);
+
+  for (var i = 0; i < airdata.length; i++) {
+    // format repo name
+    //var airDataName = [i].owner.login + "/" + repos[i].name;
+    //console.log("Air data" + airdata[i]);
+    // create a container for each repo
+  }
+};
+
+var getHikeData = function () {
+  //console.log("Getting here");
+  var apiUrl =
+    "https://www.hikingproject.com/data/get-trails?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&maxDistance=10&key=200920899-e14060291ce05aae74df93b6c9e5abc8";
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayHikeData(data);
+       // console.log(data);
+       
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+};
+// getHikeData();
+
+var displayHikeData = function (hikeData) {
+  
+  
+
+  // check if api returned any repos
+
+  if (hikeData.length === 0) {
+    //repoContainerEl.textContent = "No repositories found.";
+    alert("No repositories found");
+    return;
+  }
+  for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+        // do whatever you want with the checked radio
+        //alert(radios[i].value);
+        requestedDifficulty=radios[i].value;
+        console.log("Requested Difficulty")
+        console.log(requestedDifficulty)
+        if(requestedDifficulty==="medium"){
+          difficulty="blueBlack"
+          //console.log("medium = blueblack")
+          //console.log(difficulty)
+        }else if(requestedDifficulty==="easy"){
+          difficulty="blue"
+        }
+        else{
+          difficulty="black"
+        }
+console.log(difficulty)
+
+        // only one radio can be logically checked, don't check the rest
+        break;
+    }
+    //console.log("final")
+//console.log("difficulty")
+}
+
+//console.log(difficulty)
+  //console.log(hikeData);
+
+  // loop over repos
+  for (var i = 0; i < hikeData.trails.length; i++) {
+    // display images
+    var trailDifficulty = hikeData.trails[i].difficulty;
+    
+    if (trailDifficulty===difficulty){
+      //console.log("equal")
+   
+    //console.log("Trail Difficulty")
+    console.log(trailDifficulty)
+    console.log(difficulty)
+    var img = document.createElement("img");
+    var imgSrc = hikeData.trails[i].imgSmallMed;
+    // console.log(imgSrc + "SUP FOO");
+    img.setAttribute("src", imgSrc);
+    img.setAttribute("alt", "Image not available");
+
+    //display location
+    var cityNameDiv = document.createElement("div");
+    var cityName = hikeData.trails[i].location;
+    cityNameDiv.setAttribute("class", "city-name");
+    cityNameDiv.textContent = cityName;
+
+    //display trail name
+    var trailNameDiv = document.createElement("div");
+    var trailName = hikeData.trails[i].name;
+    trailNameDiv.setAttribute("class", "trail-name");
+    trailNameDiv.textContent = trailName;
+
+    //display trail length
+    var trailLengthDiv = document.createElement("div");
+    var trailLength = hikeData.trails[i].length;
+    trailLengthDiv.setAttribute("class", "trail-length");
+    trailLengthDiv.textContent = trailLength + " miles";
+
+    //display trail difficulty
+    var trailDifficultyDiv = document.createElement("div");
+    var trailDifficulty = hikeData.trails[i].difficulty;
+    trailDifficultyDiv.setAttribute("class", "trail-difficulty");
+    trailDifficultyDiv.textContent = trailDifficulty;
+
+    resultsBox = document.querySelector("#results");
+
+    resultsBox.prepend(trailDifficultyDiv);
+    resultsBox.prepend(trailLengthDiv);
+    resultsBox.prepend(trailNameDiv);
+    resultsBox.prepend(cityNameDiv);
+    resultsBox.prepend(img);
+  }
+  //var Data1 = "Air Data 1"
+  // console.log(hikeData.trails[0].name);
+  // create a span element to hold repository name
+  var titleEl = document.createElement("span");
+   titleEl.textContent = hikeData.trails[0].name;
+  //console.log("Finally got here");
+
+  // search results display
+  document.querySelector("#resultRow").style.display = "";
+};
+}
+locationInputEl.addEventListener("submit", function (event) {
+  event.preventDefault();
+  resultsBox.innerHTML = "";
+  //console.log("I submitted " + inputEl.value.trim());
+  getAirQuality(inputEl.value.trim());
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+      
